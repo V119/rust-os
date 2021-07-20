c72e627bd0a011958ddbd664c62c7c9f79255f2d
yanghao <928131755@qq.com>
2021-07-20 19:32:40 +0800

    20210720 第四章 2

---
+++ test/basic_boot.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplement!();
+}
+
+#[panic_handler]
+fn panic_handler(info: &PanicInfo) -> ! {
+    loop {}
+}
